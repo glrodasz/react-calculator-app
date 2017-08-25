f70dbd99c8da0bb71379fe2ca367db6336ec9dd5
Guillermo Rodas <garethderioth@gmail.com>
2017-08-25 14:24:42 -0500

    Append values

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,42 +7,41 @@ class Calculator extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { currentValue: ''};
+    this.state = { currentValue: '' };
   }
 
-  handleClick = (value) => {
-    const { currentValue } = this.state;
-
-    return (event) => {
-      this.setState({ currentValue: `${currentValue}${value}` });
-    }
-  }
+  appendValue = event => {
+    const value = event.currentTarget.dataset.text;
+    this.setState({ currentValue: `${this.state.currentValue}${value}` });
+  };
 
   render() {
     return (
       <div className="calculator">
-      	<div className="top">
-          <Key text="CLEAR" className="clear" />
-      		<div className="screen">{this.state.currentValue}</div>
-      	</div>
-      	<div className="keys">
-          <Key text={7} handleClick={this.handleClick('7')} />
-          <Key text={8} handleClick={this.handleClick('8')} />
-          <Key text={9} handleClick={this.handleClick('9')} />
-          <Key text="+" className="operator" />
-          <Key text={4} />
-          <Key text={5} />
-          <Key text={6} />
-          <Key text="-" className="operator" />
-          <Key text={1} />
-          <Key text={2} />
-          <Key text={3} />
-          <Key text="÷" className="operator" />
-      		<Key text={0} />
-      		<Key text="." />
-          <Key text="=" className="eval" />
-          <Key text="x" className="operator" />
-      	</div>
+        <div className="top">
+          <Key text="CLEAR" handleClick={this.appendValue} type="clear" />
+          <div className="screen">
+            {this.state.currentValue}
+          </div>
+        </div>
+        <div className="keys">
+          <Key text={7} handleClick={this.appendValue} />
+          <Key text={8} handleClick={this.appendValue} />
+          <Key text={9} handleClick={this.appendValue} />
+          <Key text="+" handleClick={this.appendValue} type="operator" />
+          <Key text={4} handleClick={this.appendValue} />
+          <Key text={5} handleClick={this.appendValue} />
+          <Key text={6} handleClick={this.appendValue} />
+          <Key text="-" handleClick={this.appendValue} type="operator" />
+          <Key text={1} handleClick={this.appendValue} />
+          <Key text={2} handleClick={this.appendValue} />
+          <Key text={3} handleClick={this.appendValue} />
+          <Key text="÷" handleClick={this.appendValue} type="operator" />
+          <Key text={0} handleClick={this.appendValue} />
+          <Key text="." handleClick={this.appendValue} />
+          <Key text="=" handleClick={this.appendValue} type="eval" />
+          <Key text="x" handleClick={this.appendValue} type="operator" />
+        </div>
       </div>
     );
   }
--- src/components/Key.js
+++ src/components/Key.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 import '../styles/Key.css';
 
-const Key = ({ text, className, handleClick }) =>
-  <span onClick={handleClick} className={className}>
+const Key = ({ text, type, handleClick }) =>
+  <span onClick={handleClick} className={type} data-text={text}>
     {text}
   </span>;
 
