6506d5d69b4760c1fb586c818fa9c5ef2abe71ad
Guillermo Rodas <garethderioth@gmail.com>
2017-08-30 13:26:39 -0500

    Functional Calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,37 +10,46 @@ class Calculator extends Component {
     this.state = { currentValue: '' };
   }
 
-  appendValue = event => {
+  appendValueToState = event => {
     const value = event.currentTarget.dataset.text;
     this.setState({ currentValue: `${this.state.currentValue}${value}` });
   };
+
+  evalCurrentState = event => {
+    const { currentValue } = this.state;
+    this.setState({ currentValue: eval(currentValue.replace(/÷/g, '/').replace(/x/g, '*')) });
+  }
+
+  clearCurrentState = event => {
+    this.setState({ currentValue: '' });
+  }
 
   render() {
     return (
       <div className="calculator">
         <div className="top">
-          <Key text="CLEAR" handleClick={this.appendValue} type="clear" />
+          <Key text="CLEAR" handleClick={this.clearCurrentState} type="clear" />
           <div className="screen">
             {this.state.currentValue}
           </div>
         </div>
         <div className="keys">
-          <Key text={7} handleClick={this.appendValue} />
-          <Key text={8} handleClick={this.appendValue} />
-          <Key text={9} handleClick={this.appendValue} />
-          <Key text="+" handleClick={this.appendValue} type="operator" />
-          <Key text={4} handleClick={this.appendValue} />
-          <Key text={5} handleClick={this.appendValue} />
-          <Key text={6} handleClick={this.appendValue} />
-          <Key text="-" handleClick={this.appendValue} type="operator" />
-          <Key text={1} handleClick={this.appendValue} />
-          <Key text={2} handleClick={this.appendValue} />
-          <Key text={3} handleClick={this.appendValue} />
-          <Key text="÷" handleClick={this.appendValue} type="operator" />
-          <Key text={0} handleClick={this.appendValue} />
-          <Key text="." handleClick={this.appendValue} />
-          <Key text="=" handleClick={this.appendValue} type="eval" />
-          <Key text="x" handleClick={this.appendValue} type="operator" />
+          <Key text={7} handleClick={this.appendValueToState} />
+          <Key text={8} handleClick={this.appendValueToState} />
+          <Key text={9} handleClick={this.appendValueToState} />
+          <Key text="+" handleClick={this.appendValueToState} type="operator" />
+          <Key text={4} handleClick={this.appendValueToState} />
+          <Key text={5} handleClick={this.appendValueToState} />
+          <Key text={6} handleClick={this.appendValueToState} />
+          <Key text="-" handleClick={this.appendValueToState} type="operator" />
+          <Key text={1} handleClick={this.appendValueToState} />
+          <Key text={2} handleClick={this.appendValueToState} />
+          <Key text={3} handleClick={this.appendValueToState} />
+          <Key text="÷" handleClick={this.appendValueToState} type="operator" />
+          <Key text={0} handleClick={this.appendValueToState} />
+          <Key text="." handleClick={this.appendValueToState} />
+          <Key text="=" handleClick={this.evalCurrentState} type="eval" />
+          <Key text="x" handleClick={this.appendValueToState} type="operator" />
         </div>
       </div>
     );
